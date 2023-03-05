78bfee94ae1acc61c83c0cc1d2b5608358c3031c
Bhuneshvar <imankitkalirawana@gmail.com>
2023-03-05 16:48:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
 /** @format */
+// sticky navbar on scroll script
 $(window).scroll(function () {
-  // sticky navbar on scroll script
   if (this.scrollY > 80) {
     $(".header").addClass("sticky");
   } else {
     $(".header").removeClass("sticky");
   }
 });
-
-// owl carousel script
-$(".carousel1").owlCarousel({
-  margin: 20,
-  loop: true,
-  autoplay: true,
-  autoplayTimeOut: 2000,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: false,
-    },
-    800: {
-      items: 2,
-      nav: false,
-    },
-    1000: {
-      items: 3,
-      nav: false,
-    },
-    1200: {
-      items: 4,
-      nav: false,
-    },
-  },
+$(document).ready(function () {
+  $(".member-btn-1").click(function () {
+    $(".team-details-1").slideToggle();
+    $(".team-details-2").slideUp();
+    $(".team-details-3").slideUp();
+    $(".member-btn-1 .fa-caret-down").toggleClass("rotate");
+    $(".member-btn-2 .fa-caret-down").removeClass("rotate");
+    $(".member-btn-3 .fa-caret-down").removeClass("rotate");
+  });
+  $(".member-btn-2").click(function () {
+    $(".team-details-2").slideToggle();
+    $(".team-details-1").slideUp();
+    $(".team-details-3").slideUp();
+    $(".member-btn-1 .fa-caret-down").removeClass("rotate");
+    $(".member-btn-2 .fa-caret-down").toggleClass("rotate");
+    $(".member-btn-3 .fa-caret-down").removeClass("rotate");
+  });
+  $(".member-btn-3").click(function () {
+    $(".team-details-3").slideToggle();
+    $(".team-details-1").slideUp();
+    $(".team-details-2").slideUp();
+    $(".member-btn-1 .fa-caret-down").removeClass("rotate");
+    $(".member-btn-2 .fa-caret-down").removeClass("rotate");
+    $(".member-btn-3 .fa-caret-down").toggleClass("rotate");
+  });
 });
